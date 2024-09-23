4b977823afbf4b96ed992ecdeff8e12e2ae26b9b
MAEDA AYUMU <>
2024-09-23 09:09:24 +0900

    profileモデルの作成 マイグレーションファイルの実行 ユーザーとプロフィールの関連付け

---
+++ app/models/profile.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :bigint           not null, primary key
+#  birthday     :date
+#  gender       :integer
+#  introduction :text
+#  nickname     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+class Profile < ApplicationRecord
+  belongs_to :user
+end
---
+++ test/models/profile_test.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :bigint           not null, primary key
+#  birthday     :date
+#  gender       :integer
+#  introduction :text
+#  nickname     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+require "test_helper"
+
+class ProfileTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
