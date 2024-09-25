8f66e7ea024ab76b2d1f37bb0a5145db1d66273a
MAEDA AYUMU <>
2024-09-25 23:08:28 +0900

    createアクション showアクション showのビューを作成 CSSの修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,12 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
   end
 
   def show
+    #set_article
   end
 
   def new
@@ -11,6 +14,14 @@ class BoardsController < ApplicationController
   end
 
   def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board),notice: '保存できたよ' #保存成功時に詳細ページへ
+    else
+      puts @board.errors.full_messages #エラーメッセージをコンソールに出力
+      flash.now[:error] = '保存に失敗しました' #失敗時にエラーメッセージを一時表示
+      render :new #フォームを再表示(入力データは保持)
+    end
   end
 
   def edit
@@ -20,6 +31,15 @@ class BoardsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :description)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 
 end
