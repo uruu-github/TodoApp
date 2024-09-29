553bf64c1604153e08a1213ac9014497f6e1317b
MAEDA AYUMU <>
2024-09-29 15:51:11 +0900

    フォームに画像添付用のコードを追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,4 +10,17 @@ class ProfilesController < ApplicationController
 
   def update
   end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed,
+      :avatar
+    )
+  end
+  
 end
