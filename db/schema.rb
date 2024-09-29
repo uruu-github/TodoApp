f5696b1491fa0197259bddcf8b38d42cc358f52f
MAEDA AYUMU <>
2024-09-29 15:17:32 +0900

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_233704) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_23_054247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
