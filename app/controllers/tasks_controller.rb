2a2982329b84b4b54c255a32fece8b6479f1ceb8
MAEDA AYUMU <>
2024-10-01 00:57:47 +0900

    index画面の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @tasks = Task.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :boards
   resources :tasks
 
+
   resource :profile, only: [:show, :edit, :update]
 
 end
