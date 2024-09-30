525c27dd083b8aef33bfa1a93c2b2d9045793694
MAEDA AYUMU <>
2024-09-30 09:11:05 +0900

    細かな修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :boards
+  has_many :boards, dependent: :destroy
 
   has_one :profile, dependent: :destroy
 
