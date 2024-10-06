925583640728abcb18039484ce6b317b5e83d725
MAEDA AYUMU <>
2024-10-06 13:07:00 +0900

    routesの設定（さっきのは記述が間違えてた）

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   resources :boards do
     resources :tasks
-    resources :comments, only[:new, :create]
+    resources :comments, only: [:new, :create]
   end
 
 
