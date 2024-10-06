70379c73b2f6ca2a20f3a2863142f578e0539edb
MAEDA AYUMU <>
2024-10-07 07:58:38 +0900

    no message

--- db/migrate/20240930083347_fix_tasks_association.rb
+++
@@ -1,9 +0,0 @@
-class FixTasksAssociation < ActiveRecord::Migration[7.2]
-  def change
-    # # 外部キー制約なしでboard_idカラムを削除
-    # remove_reference :tasks, :board
-
-    # 正しいカラムを追加
-    add_reference :tasks, :user, null: false, foreign_key: true
-  end
-end
